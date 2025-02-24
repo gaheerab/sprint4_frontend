32f89af8daa5599c1bae542575306b161d1081c4
hannahli11 <hannahli16492@gmail.com>
2025-02-24 11:50:51 -0800

    8404

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,8 +1,8 @@
 export var pythonURI;
 if (location.hostname === "localhost") {
-        pythonURI = "http://localhost:8887";
+        pythonURI = "http://localhost:8404";
 } else if (location.hostname === "127.0.0.1") {
-        pythonURI = "http://127.0.0.1:8887";
+        pythonURI = "http://127.0.0.1:8404";
 } else {
         pythonURI =  "https://melodymates.stu.nighthawkcodingsociety.com";
 }
